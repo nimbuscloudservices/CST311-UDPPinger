0e178c7deb92c1ac70dc8b80b427c5f88649247a
Saul M <smendozaloera@csumb.edu>
2022-05-11 15:42:31 -0700

    Added start/end time

--- Client.py
+++ Client.py
@@ -1,19 +1,22 @@
+import time
 from socket import *
 serverName = '10.0.0.2'
 serverPort = 12000
 clientSocket = socket(AF_INET,SOCK_DGRAM)
 clientSocket.settimeout(1)
 
-for  i in range(1,11):
+for i in range(1,11):
 
   try:
     message = 'Ping'+ str(i)
+    startTime = time.time()
     clientSocket.sendto(message.encode(),(serverName,serverPort))
     modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+    returnTime = time.time()
     print('Mesg sent:', 'Ping'+ str(i))
     print('Mesg rcvd:', modifiedMessage.decode())
-    print('Start time:')
-    print('Return time:')
+    print('Start time:', '{:e}'.format(startTime))
+    print('Return time:', '{:e}'.format(returnTime))
     print('PONG', i, 'RTT: \n')
   except:
     print('Mesg sent:', 'Ping' + str(i))
