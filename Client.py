9b15058bc14a3475d5e37704cc09d921cc4a7a46
Yavik-Kapadia <yavikkapadia@gmail.com>
2022-05-11 21:57:49 -0700

    Implemented EstimatedRTT, DevRTT, and Timeout interval calculation.

--- Client.py
+++ Client.py
@@ -4,9 +4,11 @@ from socket import *
 host = '10.0.0.2'
 port = 12000
 timeout = 1  # in seconds
-#used for stats
+# used for stats
 alpha = 0.125
 beta = 0.25
+estimatedRTT = 0
+devRTT = 0
 # Creates UDP client socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 # sets socket timeout to 1 second
@@ -29,24 +31,38 @@ for i in range(1, 11):
         rtt = (returnTime - sendTime) * 1000
         # Add rtt to array
         rtt_stats.append(rtt)
+        if i == 1:
+            estimatedRTT = rtt
+            devRTT = rtt / 2
+        else:
+            estimatedRTT = ((1 - alpha) * estimatedRTT) + (alpha * rtt)
+            devRTT = ((1 - beta) * devRTT) + (beta * abs(rtt - estimatedRTT))
 
         print('Mesg sent:', data)
         print('Mesg rcvd:', modifiedMessage.decode())
         print('Start time:', '{:e}'.format(sendTime))
         print('Return time:', '{:e}'.format(returnTime))
         print('PONG {} RTT: {} ms\n'.format(i, rtt))
-    except:
+    except timeout:
         print('PONG', i, 'Request timed out \n')
         continue
 clientSocket.close()
 
-# Stats
-loss = ((10 - len(rtt_stats))/10)*100
+# Calculate Stats
+# Packet Loss
+loss = ((10 - len(rtt_stats)) / 10) * 100
+# Timeout interval value
+timeoutInterval = estimatedRTT + (4 * devRTT)
+# Average
+avg = sum(rtt_stats) / len(rtt_stats)
 
+# Print out Stats
+print('-------------- Ping Stats ---------------')
 print('Min RTT: {}'.format(min(rtt_stats)))
 print('Max RTT: {}'.format(max(rtt_stats)))
-print('Avg RTT: {}'.format(sum(rtt_stats) / len(rtt_stats)))
+print('Avg RTT: {}'.format(avg))
 print('Packet Loss: {}%'.format(loss))
-print('Estimated RTT:')
-print('Dev RTT:')
-print('Timeout Interval:')
+print('Estimated RTT: {} ms'.format(estimatedRTT))
+print('Dev RTT: {} ms'.format(devRTT))
+print('Timeout Interval: {} ms'.format(timeoutInterval))
+print('-----------------------------------------')
